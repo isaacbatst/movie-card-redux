65f99ddf531a4d1f8732aeba5ce8e4a624580ab3
isaacbatst <isaacbatst@gmail.com>
2021-11-09 20:05:13 -0300

    adiciona actions

--- movie-cards-redux/src/App.js
+++ movie-cards-redux/src/App.js
@@ -2,26 +2,15 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import Player from './components/Player';
 import Sidebar from './components/Sidebar';
-import data from './data';
 import store from './store';
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      categories: [...data],
-    };
-  }
-
   render() {
-    const { categories } = this.state;
-
     return (
       <Provider store={ store }>
         <div>
           <Player />
-          <Sidebar categories={ categories } />
+          <Sidebar />
         </div>
       </Provider>
     );
--- movie-cards-redux/src/store/reducers/index.js
+++ movie-cards-redux/src/store/reducers/index.js
@@ -1,12 +1,21 @@
 import { combineReducers } from 'redux';
+import data from '../../data';
+import { SELECT_MOVIE } from '../actions';
 
 const INITIAL_STATE = {
   selectedMovie: 'Corra',
   selectedCategory: 'Atletismo',
+  categories: data,
 };
 
 const playerReducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
+  case SELECT_MOVIE:
+    return {
+      ...state,
+      selectedMovie: action.movie,
+      selectedCategory: action.category,
+    };
   default:
     return state;
   }
